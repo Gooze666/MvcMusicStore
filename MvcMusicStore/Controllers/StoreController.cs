4dcfc7644b379ebf8c1ed3c8099d80c32a6afe8d
Gooze <Gooze@GOOZE-PC>
2016-05-02 16:36:29 +0200

    Model eingebunden

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
@@ -11,7 +12,25 @@ namespace MvcMusicStore.Controllers
         // GET: Store
         public ActionResult Index()
         {
-            return View();
+            var genres = new List<Genre>
+            {
+                new Genre {Name = "Disco" },
+                new Genre {Name = "Jazz" },
+                new Genre {Name = "Rock" }
+            };
+            return View(genres);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
         }
     }
 }
